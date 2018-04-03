be2aa1b7a73ca4bb873576cc59c41c8e09f2e6f8
mwood <matt@cratebind.com>
2018-04-03 15:02:35 -0500

    entered custom input of database name

--- index.js
+++ index.js
@@ -9,30 +9,33 @@ const chalk = require('chalk');
 // var userArgs = process.argv.slice(2);
 
 try {
-  // fetch database names
-  const databaseConfig = fs.readFileSync('./config/database.yml', 'utf8');
+  let databaseNames;
 
-  // map into array of names
-  const databaseNames = databaseConfig
-    .split('\n')
-    .filter(line => line.includes('database'))
-    .map(name => name.split(': ')[1]);
+  // check if database exists in config
+  if (fs.existsSync('./config/database.yml')) {
+    // fetch database names
+    const databaseConfig = fs.readFileSync('./config/database.yml', 'utf8');
+
+    // map into array of names
+    databaseNames = databaseConfig
+      .split('\n')
+      .filter(line => line.includes('database'))
+      .map(name => name.split(': ')[1]);
+  } else {
+    // if database.yml doesn't exist, set databaseNames to empty array and prompt later
+    databaseNames = null;
+  }
 
   // fetch dump files
   const files = fs.readdirSync('.');
 
   // filter root files by .dump extension
-  const dumpFiles = files.filter(file =>
+  const dumpFiles = files.filter(file => {
     // return all dump files
-    file.includes('.dump'));
+    file.includes('.dump');
+  }) || [];
 
   const questions = [
-    {
-      type: 'list',
-      name: 'database',
-      message: 'Which database do you want to restore?',
-      choices: databaseNames
-    },
     {
       type: 'list',
       name: 'dumpFile',
@@ -41,7 +44,33 @@ try {
     }
   ];
 
-  if (databaseNames.length && dumpFiles.length) {
+  // if database names and dump files are both found, start prompts
+  if (databaseNames && dumpFiles.length) {
+    // add database question to beginning of questions
+    questions.unshift({
+      type: 'list',
+      name: 'database',
+      message: 'Which database do you want to restore?',
+      choices: databaseNames
+    });
+
+    // start prompts
+    inquirer.prompt(questions).then((answers) => {
+      const selectedDatabase = answers.database;
+      const selectedDumpFile = answers.dumpFile;
+      // execute command
+      shell.exec(`pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d ${selectedDatabase} ${selectedDumpFile}`);
+      console.log(chalk.green(`Successfully restored database ${selectedDatabase}`));
+    });
+  } else if (databaseNames === null && dumpFiles.length) {
+    console.log(chalk.black.bgRed("Couldn't find database.yml"));
+
+    questions.unshift({
+      type: 'input',
+      name: 'database',
+      message: 'Please Enter Your Database Name'
+    });
+
     inquirer.prompt(questions).then((answers) => {
       const selectedDatabase = answers.database;
       const selectedDumpFile = answers.dumpFile;
@@ -50,12 +79,12 @@ try {
       console.log(chalk.green(`Successfully restored database ${selectedDatabase}`));
     });
   } else {
-    console.log(chalk.black.bgRed('Missing Resources:'));
-    databaseNames.length === 0
-      ? console.log(chalk('Database Config (should be at ./config/database.yml)'))
+    console.log(chalk.black.bgRed('PG Restore Failed:'));
+    !databaseNames
+      ? console.log(chalk('- Cannot Find Database Config (should be at ./config/database.yml)'))
       : null;
     dumpFiles.length === 0
-      ? console.log(chalk('Dump Files (should be in project root directory)'))
+      ? console.log(chalk('- No Dump Files Found (should be in project root directory)'))
       : null;
   }
 } catch (error) {
